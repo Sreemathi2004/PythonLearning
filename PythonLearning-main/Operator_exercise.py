590b57ddf63be40bbea27d7d4c60878836edfdde
Sreemathi2004 <sreemathik.m2004@gmail.com>
2024-02-28 09:28:32 +0530

    updated

---
+++ PythonLearning-main/Operator_exercise.py
@@ -0,0 +1,8 @@
+age=int(input("Enter your age"))
+years_left=90-age;
+# find how many days left
+days_left=years_left*365
+months_left=years_left*12
+weeks_left=years_left*52
+
+print(f"days left {days_left} months left {months_left} weeks left {weeks_left}")
